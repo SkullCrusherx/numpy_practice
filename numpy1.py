f5c63f44fdde64a1cdfe597e69bd766f87669617
Predator <kungfuyoga99@gmail.com>
2026-02-27 21:40:29 +0530

    update

--- numpy1.py
+++ numpy1.py
@@ -5,5 +5,10 @@ x = [1,2,3,4,5,6] #list
 y = np.array(x) #make list into array
 print(y) #show output
 
+lst = [] #empty list
+for i in range(6):
+    user = int(input(" enter new input : "))
+    lst.append(user)
 
+print(np.array(lst))
 
