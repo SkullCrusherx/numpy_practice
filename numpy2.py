2bcfb6cc117e439110be5e79c8702664e384806c
Predator <kungfuyoga99@gmail.com>
2026-02-27 22:02:26 +0530

    update

--- numpy2.py
+++ numpy2.py
@@ -1,8 +1,9 @@
 #creating numpy array using numpy function
-
 # 0's arra fill special Character
-
 import numpy as np
-
 ar = np.zeros(4)
-print(ar)
+print(ar) #single Dimension
+arm = np.zeros((3,4))
+print(arm) # multi Dimension
+# 1 's dimension same like zero
+
