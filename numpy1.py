21ea90794697a98b69708f0d31d50c56acc4dab3
Predator <kungfuyoga99@gmail.com>
2026-02-27 21:36:53 +0530

    update

--- numpy1.py
+++ numpy1.py
@@ -1,10 +1,9 @@
 #Array Creating
 
-import numpy as np
+import numpy as np # import library
+x = [1,2,3,4,5,6] #list
+y = np.array(x) #make list into array
+print(y) #show output
 
-x = [1,2,3,4,5,6]
-
-y = np.array(x)
-print(y)
 
 
