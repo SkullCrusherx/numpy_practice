6d45e613add05fa0ee7bee8230924d4a9cf85699
Predator <kungfuyoga99@gmail.com>
2026-02-27 22:15:40 +0530

    update

--- numpy2.py
+++ numpy2.py
@@ -7,6 +7,12 @@ arm = np.zeros((3,4))
 print(arm) # multi Dimension
 # 1 's dimension same like zero
 #empty also like this using np.empty() its shows prevous element may be show
+# range using like for loop
+m = np.arange(1,10,2)
+print(m)
 
+# Diagonal fill with ones like
+arr_Dig = np.eye(7)
+print(arr_Dig)
 
 
