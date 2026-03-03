170dc7b718cdd6baa890bd2a4a787f68c5935fac
Predator <kungfuyoga99@gmail.com>
2026-03-03 19:24:10 +0530

    update

--- numpy4.py
+++ numpy4.py
@@ -8,5 +8,8 @@ print(var.dtype) #check the datatype int64
 var_f  = np.array([1.0,2.0,3.9,4.0])
 print(var_f.dtype) #check the datatype float64
 
-var_f  = np.array(["A","B","C","D"])
-print(var_f.dtype) #check the datatype <u1 for string
+var_s  = np.array(["A","B","C","D"])
+print(var_s.dtype) #check the datatype <u1 for string
+
+var_u  = np.array(["A","B","C","D",1 ,2, 3, 4])
+print(var_u.dtype) #check the datatype <u21 for string
