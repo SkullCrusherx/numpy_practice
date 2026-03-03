7f92c32d82b1e1ed6d08a73e621c9d8a14c7739e
Predator <kungfuyoga99@gmail.com>
2026-03-03 21:37:48 +0530

    update

--- numpy5.py
+++ numpy5.py
@@ -1,3 +1,10 @@
 #arethmetic operation in numpy
 import numpy as np
 
+var_1 =np.array([1,2,3,4])
+print(var_1+3)
+
+#array + array adding
+var_2 =np.array([1,2,3,4])
+var_3 = np.array([1,2,3,4])
+print(var_2+var_3)
