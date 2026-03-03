97fd416214879c89feeebae1ec6f1ac6bdf3c033
Predator <kungfuyoga99@gmail.com>
2026-03-03 19:22:59 +0530

    update

--- numpy4.py
+++ numpy4.py
@@ -3,7 +3,10 @@ import numpy as np
 from numpy import dtype
 
 var  = np.array([1,2,3,4])
-print(var.dtype) #check the datatype
+print(var.dtype) #check the datatype int64
 
 var_f  = np.array([1.0,2.0,3.9,4.0])
-print(var_f.dtype) #check the datatype
+print(var_f.dtype) #check the datatype float64
+
+var_f  = np.array(["A","B","C","D"])
+print(var_f.dtype) #check the datatype <u1 for string
