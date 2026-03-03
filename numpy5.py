480f0779f0fe56c2890b22d198ea39dc0532c38f
Predator <kungfuyoga99@gmail.com>
2026-03-03 21:39:33 +0530

    update

--- numpy5.py
+++ numpy5.py
@@ -1,6 +1,7 @@
 #arethmetic operation in numpy
 import numpy as np
 
+#addition only single array with int
 var_1 =np.array([1,2,3,4])
 print(var_1+3)
 
@@ -8,3 +9,12 @@ print(var_1+3)
 var_2 =np.array([1,2,3,4])
 var_3 = np.array([1,2,3,4])
 print(var_2+var_3)
+
+#subtract with int
+var_1 =np.array([1,2,3,4])
+print(var_1-3)
+
+#subtract array from array
+var_2 =np.array([1,2,3,4])
+var_3 = np.array([1,2,3,4])
+print(var_2-var_3)
