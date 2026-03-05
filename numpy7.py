3c50aad105c19ad33ce48230fd9343c86fcb86a3
Predator <kungfuyoga99@gmail.com>
2026-03-05 23:06:35 +0530

    update

--- numpy7.py
+++ numpy7.py
@@ -1,4 +1,12 @@
 import numpy as np
+"""var = np.array([1,2,3,4])
+print(var.shape)
+var_1 = np.ones((2,4,3))
+print(var_1)"""
 
-var = np.array([[1,2,3],[4,5,6],[7,8,9]])
-print(np.transpose(var))
+
+
+# Create a 3D array with ones along the second axis
+arr = np.ones((2, 3, 4), dtype=int)
+arr[:, 1, :] = 2
+print(arr)
