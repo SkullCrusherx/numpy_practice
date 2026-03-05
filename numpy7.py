cfebc1f36e52a02d2cef72ddb93aca415e218c8a
Predator <kungfuyoga99@gmail.com>
2026-03-05 23:20:08 +0530

    update

--- numpy7.py
+++ numpy7.py
@@ -1,12 +1,11 @@
 import numpy as np
-"""var = np.array([1,2,3,4])
-print(var.shape)
-var_1 = np.ones((2,4,3))
-print(var_1)"""
+
+var = np.array([1,2,3,4])
 
 
+var_1 = np.ones((1,2,3,2,5,7),dtype=np.int8)
+print(var_1)
+print(var_1.ndim)
+print(var_1.shape)
 
-# Create a 3D array with ones along the second axis
-arr = np.ones((2, 3, 4), dtype=int)
-arr[:, 1, :] = 2
-print(arr)
+
