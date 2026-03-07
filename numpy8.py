07fa57dcd4633da6f4111d4bdfd8d1cf06d5c28a
Predator <kungfuyoga99@gmail.com>
2026-03-08 00:14:56 +0530

    update

--- numpy8.py
+++ numpy8.py
@@ -3,7 +3,9 @@ import numpy as np
 var = np.array([1,2,3,4,5,6,7,8,9,10,11,12])
 #checking the shape
 f = var.shape
-print(f)
+print(f"shape of var : \n",f)
 
+
+#3d shape making
 var_reshape = var.reshape(2,3,2)
-print("3D making 1# :",f"\n",var_reshape)
+print(f"3D making 1# : \n",var_reshape)
