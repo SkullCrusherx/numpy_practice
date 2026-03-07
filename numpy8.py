0f90f419ec80d041b6efbc68688cc59a77142f86
Predator <kungfuyoga99@gmail.com>
2026-03-08 00:16:34 +0530

    update

--- numpy8.py
+++ numpy8.py
@@ -5,7 +5,13 @@ var = np.array([1,2,3,4,5,6,7,8,9,10,11,12])
 f = var.shape
 print(f"shape of var : \n",f)
 
+#2D shape making
+var_2d = var.reshape(3,4)
+var_2d = var.reshape(6,2)
+var_2d = var.reshape(4,3)
+print(f"shape of var_2d : \n",var_2d)
+
 
 #3d shape making
-var_reshape = var.reshape(2,3,2)
-print(f"3D making 1# : \n",var_reshape)
+var_3d = var.reshape(2,3,2)
+print(f"3D making 1# : \n",var_3d)
