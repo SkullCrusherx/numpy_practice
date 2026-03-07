e44dc373efcbcbdb9d7569cc36d4dacd1a7e4ded
Predator <kungfuyoga99@gmail.com>
2026-03-07 08:36:53 +0530

    update

--- numpy7.py
+++ numpy7.py
@@ -7,5 +7,10 @@ print(var.shape)
 var_1 = np.array([[1,2,3],[3,4,5]])
 
 print(var_1.shape) #show the shape
+#for multi dimension regarding
+
+var_multi = np.array([1,2,3,4],ndmin=5)
+print(var_multi)
+print(var_multi.shape)
 
 
