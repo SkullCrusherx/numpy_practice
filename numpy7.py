247a339d1faab527bb7e11c0d7ea57f3223e60bf
Predator <kungfuyoga99@gmail.com>
2026-03-07 09:18:42 +0530

    update

--- numpy7.py
+++ numpy7.py
@@ -35,4 +35,6 @@ print("2D making 2# :",var_reshape)
 var_reshape = var_shape.reshape(3,2,2)
 print("3D making 1# :",var_reshape)
 
+#reshape multi Dimension To 1D 2D or 3D
+print(var_1.reshape(-1))
 
