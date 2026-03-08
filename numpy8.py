50e8a55e727b000b9e85af0b0c3efe2beff6b8ee
Predator <kungfuyoga99@gmail.com>
2026-03-08 18:45:00 +0530

    update

--- numpy8.py
+++ numpy8.py
@@ -1,3 +1,7 @@
 #BroadCasting
 
 import numpy as np
+
+# 1) check dimension and element if both equal its goes work
+# 2) check always shape is shape from right side compare if same like Var_1 coloumn is 1 or n and
+# var_2 is 1 or n if both same or one of them 1 its work
