3ac85fbfd17829a92602b1c675f7d0045fa14c74
Predator <kungfuyoga99@gmail.com>
2026-03-08 18:46:46 +0530

    update

--- numpy8.py
+++ numpy8.py
@@ -5,3 +5,12 @@ import numpy as np
 # 1) check dimension and element if both equal its goes work
 # 2) check always shape is shape from right side compare if same like Var_1 coloumn is 1 or n and
 # var_2 is 1 or n if both same or one of them 1 its work
+
+
+var_1 = np.array([1,2,3,4,5])
+var_2 = np.array([1,2,3,4,5])
+#both same possible done dimension hcek using ndim and shape check using shape
+print(var_1.ndim)
+print(var_1.shape)
+print(var_2.ndim)
+print(var_2.shape)
