b59ddb0450bf24f378ce404014c4fc474a392829
Predator <kungfuyoga99@gmail.com>
2026-03-08 18:53:57 +0530

    update

--- numpy8.py
+++ numpy8.py
@@ -15,9 +15,14 @@ print(var_2.shape)
 #arethemetic operation done here
 
 #if dimension same shape is different like these
-
+var_1 = np.array([1,2,3,4,5])
+var_2 = np.array([1,2,3,4])
+print(var_1+var_2)
+#arthemetic operation error show broadcast error shape error
 
 #if dimension are different check shape
 var_1 = np.array([1,2,3,4,5,6])
 var_2 = np.array([[1,2,3],[4,5,6]])
-#
+#check shape from right side or shape is both compare
+# check right axis1 (coloumn) one of them 1 or both same like n==n
+# then compare 2nd axis (row)
