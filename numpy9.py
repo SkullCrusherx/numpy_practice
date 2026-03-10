e2c0b6e46dc96925d062a00e4c30b18027e0c12a
Predator <kungfuyoga99@gmail.com>
2026-03-10 23:55:40 +0530

    update

--- numpy9.py
+++ numpy9.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 #1D slicing
-"""var_1d = np.array([1,2,3,4,5,6,7,8,9,10])
+var_1d = np.array([1,2,3,4,5,6,7,8,9,10])
 
 # : use for after or before here number like [x:] x index number to  all show
 print(var_1d[1:])
@@ -14,7 +14,7 @@ print(var_1d[:4])
 print(var_1d[1:4])
 
 # If we need to step like 2 add [1 3 5 7] like these need to step like these [start : end : step]
-print(var_1d[0:10:2]) # here step 2"""
+print(var_1d[0:10:2]) # here step 2
 
 #------------------------------------------------------------------------
 #------------------------------------------------------------------------
@@ -51,3 +51,28 @@ print(var_2d[0,1:2:2])
 #------------------------------------------------------------------------
 #3D slicing process
 
+var_2d = np.array([[1,2,3],[4,5,6]])
+
+# : use for after or before here number like [x][x1] x is index number of which block x1 belong to block's index number
+#here I can use like this [x][x1:]
+print(var_2d[0][1:])
+print(var_2d[0,1:])
+#Both same
+
+# : use for after or before here number like [x][x1] x is index number of which block x1 belong to block's index number
+#here I can use like this [x][:x1]
+print(var_2d[0][:1])
+print(var_2d[0,:1])
+#Both same
+
+# : use for after or before here number like [x][x1] x is index number of which block x1 belong to block's index number
+#here I can use like this [x][x1(start):x1(end)]
+# [x:y] x to y all index numbers mention that means [start : end ]
+print(var_2d[0][1:2])
+print(var_2d[0,1:2])
+#Both same
+
+# If we need to step like 2 add [1 3 5 7] like these need to step like these [start : end : step]
+print(var_2d[0][1:2:2])  # here step 2
+print(var_2d[0,1:2:2])
+#Both same
