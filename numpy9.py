1a5604cb9581947934c5cf30990909c85f2479e2
Predator <kungfuyoga99@gmail.com>
2026-03-10 21:59:54 +0530

    update

--- numpy9.py
+++ numpy9.py
@@ -4,8 +4,8 @@ import numpy as np
 #1D slicing
 var = np.array([1,2,3,4,5])
 # : use for after or before here number like [x:] x index number to  all show
-# [:Y] y index number before all number
-# [x:y] x to y all index numbers mention that means [start : end ]
 print(var[1:])
+# [:Y] y index number before all number
 print(var[:4])
+# [x:y] x to y all index numbers mention that means [start : end ]
 print(var[1:4])
