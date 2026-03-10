1bca8c8897618c7099dbfcfc1b6567a439a45a55
Predator <kungfuyoga99@gmail.com>
2026-03-10 23:57:05 +0530

    update

--- numpy9.py
+++ numpy9.py
@@ -20,7 +20,7 @@ print(var_1d[0:10:2]) # here step 2
 #------------------------------------------------------------------------
 #2D slicing process
 
-var_2d = np.array([[1,2,3],[4,5,6]])
+var_2d = np.array([[1,2,3,4,5,6,7,8,9,10],[11,12,13,14,15,16,17,18,19,20]])
 
 # : use for after or before here number like [x][x1] x is index number of which block x1 belong to block's index number
 #here I can use like this [x][x1:]
@@ -51,28 +51,28 @@ print(var_2d[0,1:2:2])
 #------------------------------------------------------------------------
 #3D slicing process
 
-var_2d = np.array([[1,2,3],[4,5,6]])
+var_3d = np.array([[1,2,3,4,5,6,7,8,9,10],[11,12,13,14,15,16,17,18,19,20]])
 
 # : use for after or before here number like [x][x1] x is index number of which block x1 belong to block's index number
 #here I can use like this [x][x1:]
-print(var_2d[0][1:])
-print(var_2d[0,1:])
+print(var_3d[0][1:])
+print(var_3d[0,1:])
 #Both same
 
 # : use for after or before here number like [x][x1] x is index number of which block x1 belong to block's index number
 #here I can use like this [x][:x1]
-print(var_2d[0][:1])
-print(var_2d[0,:1])
+print(var_3d[0][:1])
+print(var_3d[0,:1])
 #Both same
 
 # : use for after or before here number like [x][x1] x is index number of which block x1 belong to block's index number
 #here I can use like this [x][x1(start):x1(end)]
 # [x:y] x to y all index numbers mention that means [start : end ]
-print(var_2d[0][1:2])
-print(var_2d[0,1:2])
+print(var_3d[0][1:2])
+print(var_3d[0,1:2])
 #Both same
 
 # If we need to step like 2 add [1 3 5 7] like these need to step like these [start : end : step]
-print(var_2d[0][1:2:2])  # here step 2
-print(var_2d[0,1:2:2])
+print(var_3d[0][1:2:2])  # here step 2
+print(var_3d[0,1:2:2])
 #Both same
