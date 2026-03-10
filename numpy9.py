96ae5037347e0bd33f43993bcd59f866480b1b27
Predator <kungfuyoga99@gmail.com>
2026-03-10 23:47:32 +0530

    update

--- numpy9.py
+++ numpy9.py
@@ -20,11 +20,19 @@ print(var_1d[0:10:2]) # here step 2
 #------------------------------------------------------------------------
 #2D slicing process
 
-var_2d = np.array([1,2,3,4,5,6,7,8,9,10])
+var_2d = np.array([[1,2,3],[4,5,6]])
 
+# : use for after or before here number like [x:] x index number to  all show
+print(var_1d[1:])
 
+# [:Y] y index number before all number
+print(var_1d[:4])
 
+# [x:y] x to y all index numbers mention that means [start : end ]
+print(var_1d[1:4])
 
+# If we need to step like 2 add [1 3 5 7] like these need to step like these [start : end : step]
+print(var_1d[0:10:2]) # here step 2
 
 
 #------------------------------------------------------------------------
