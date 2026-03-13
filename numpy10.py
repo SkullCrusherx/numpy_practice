b55c671008dbd3fea7dd6abcf28ac484431a5ca0
Predator <kungfuyoga99@gmail.com>
2026-03-14 00:44:11 +0530

    update

--- numpy10.py
+++ numpy10.py
@@ -20,3 +20,4 @@ for x in np.nditer(var):
 # If only need int or other than using casting to change it like these Casting=["unsafe"]
 for x in np.nditer(var_3d,flags=["buffered"],op_dtypes=['U'],casting="unsafe"):
     print(x)
+
