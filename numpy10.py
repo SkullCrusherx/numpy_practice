7a9250ac2e18e900d5139d317924a1b5ea1aaa62
Predator <kungfuyoga99@gmail.com>
2026-03-13 23:24:13 +0530

    update

--- numpy10.py
+++ numpy10.py
@@ -6,7 +6,7 @@ var_2d = np.array([[1,2,3,4,5,6],[9,8,7,6,5,4]])
 var_3d = np.array([[[1,2,3,4,5,6]],[[3,4,5,6,7,8]],[[4,5,6,7,8,9]]])
 
 #for 1D one for loop for 2D two for loop for 3D three for loop for 4D for four loop
-for x in var:
+for x in var_2d:
     for item in x:
         print(item)
 
@@ -18,6 +18,6 @@ for x in np.nditer(var):
 #using flags for extra space from memory
 # using OP_dtype for byte string "S"
 # If only need int or other than using casting to change it like these Casting=["unsafe"]
-for x in np.nditer(var,flags=["buffered"],op_dtypes=['U'],casting="unsafe"):
+for x in np.nditer(var_3d,flags=["buffered"],op_dtypes=['U'],casting="unsafe"):
     print(x)
 
