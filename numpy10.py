cc3e52a46cfeb5a6ea7d40c16ea57f2d200f6e75
Predator <kungfuyoga99@gmail.com>
2026-03-13 23:00:49 +0530

    update

--- numpy10.py
+++ numpy10.py
@@ -2,8 +2,8 @@
 import numpy as np
 
 var = np.array([1,2,3,4,5,6])
-var_2d = np.array([1,2,3,4,5,6])
-var_3d = np.array([1,2,3,4,5,6])
+var_2d = np.array([[1,2,3,4,5,6],[9,8,7,6,5,4]])
+var_3d = np.array([[[1,2,3,4,5,6]],[[3,4,5,6,7,8]],[[4,5,6,7,8,9]]])
 
 #for 1D one for loop for 2D two for loop for 3D three for loop for 4D for four loop
 for x in var:
