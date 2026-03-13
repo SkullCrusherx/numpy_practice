08a88130632db338e60c7364cf648f1cd6908182
Predator <kungfuyoga99@gmail.com>
2026-03-13 22:59:23 +0530

    update

--- numpy10.py
+++ numpy10.py
@@ -1,7 +1,9 @@
 # Iterration of numpy array
 import numpy as np
 
-var = np.array([[1,2,3,4,5,6],[11,12,13,14,15,16]])
+var = np.array([1,2,3,4,5,6])
+var_2d = np.array([1,2,3,4,5,6])
+var_3d = np.array([1,2,3,4,5,6])
 
 #for 1D one for loop for 2D two for loop for 3D three for loop for 4D for four loop
 for x in var:
