ec8a4c47e0d1dba842f36661051c86363d7b7cb6
Predator <kungfuyoga99@gmail.com>
2026-03-15 21:37:59 +0530

    update

--- numpy12.py
+++ numpy12.py
@@ -10,7 +10,7 @@ var_4 = np.array([[7,8,9],[7,6,5]])
 var_5 = np.array([[[1,2,3]],[[4,5,6]],[[7,8,9]]])
 var_6 = np.array([[[7,8,9]],[[2,3,4]],[[7,6,5]]])
 
-"""# for 1D joining array
+# for 1D joining array
 join = np.concatenate((var,var_1))
 print(join)
 
@@ -19,7 +19,7 @@ join_2D_row = np.concatenate((var_3,var_4),axis=1)
 join_2D_col = np.concatenate((var_3,var_4),axis=0)
 print(join_2D_row)
 print(join_2D_col)
-"""
+
 # for 3D joining array
 join_3D_row = np.concatenate((var_5,var_6),axis=1)
 join_3D_col = np.concatenate((var_5,var_6),axis=0)
