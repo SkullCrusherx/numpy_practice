584d6851e044769c53a511ab8e33d81df33c8cb1
Predator <kungfuyoga99@gmail.com>
2026-03-15 12:46:13 +0530

    update

--- numpy10.py
+++ numpy10.py
@@ -4,7 +4,7 @@ import numpy as np
 var = np.array([1,2,3,4,5,6])
 var_2d = np.array([[1,2,3,4,5,6],[9,8,7,6,5,4]])
 var_3d = np.array([[[1,2,3,4,5,6]],[[3,4,5,6,7,8]],[[4,5,6,7,8,9]]])
-
+"""
 #for 1D one for loop for 2D two for loop for 3D three for loop for 4D for four loop
 for x in var_2d:
     for item in x:
@@ -20,4 +20,9 @@ for x in np.nditer(var):
 # If only need int or other than using casting to change it like these Casting=["unsafe"]
 for x in np.nditer(var_3d,flags=["buffered"],op_dtypes=['U'],casting="unsafe"):
     print(x)
+"""
+# If we need index number also then we need to use this function and also like these
+for index,x in np.ndenumerate(var_3d):
+    print(index,x)
+
 
