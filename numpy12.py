71f2f151564598c16c4d8b5c0b96af6ce5686f51
Predator <kungfuyoga99@gmail.com>
2026-03-15 20:53:52 +0530

    update

--- numpy12.py
+++ numpy12.py
@@ -3,8 +3,14 @@ import numpy as np
 
 var = np.array([1,2,3,4])
 var_1 = np.array([6,7,8,9])
-var_3 = np.array([[123],[456]])
+var_3 = np.array([[1,2,3],[4,5,6]])
+var_4 = np.array([[7,8,9],[7,6,5]])
 
 # for 1D joining array
 join = np.concatenate((var,var_1))
+print(join)
+
+# for 1D joining array
+join = np.concatenate((var_3,var_4),axis=1)
+
 print(join)
