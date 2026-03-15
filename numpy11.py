24348672d72619f4691fd2ae9ba7a1f25eff9166
Predator <kungfuyoga99@gmail.com>
2026-03-15 15:59:58 +0530

    update

--- numpy11.py
+++ numpy11.py
@@ -1,11 +1,11 @@
 #Copy and View
 import numpy as np
 
+#copy function direct copy original function andif changes doesnt represent it
 var = np.array([1,2,3,4,5,6])
-var_copy = var.copy()
+var_copy = var.copy() #original copy
 
 var[0]=19
-
 print(var_copy)
 
 
