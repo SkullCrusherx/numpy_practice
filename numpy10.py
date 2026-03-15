f5baf843ba1e562573ebe63cf16f95349a9cf08c
Predator <kungfuyoga99@gmail.com>
2026-03-15 13:03:42 +0530

    update

--- numpy10.py
+++ numpy10.py
@@ -24,4 +24,3 @@ for x in np.nditer(var_3d,flags=["buffered"],op_dtypes=['U'],casting="unsafe"):
 # If we need index number also then we need to use this function and also like these
 for index,x in np.ndenumerate(var_3d): #here index contains the index number of that
     print(index,x)
-
