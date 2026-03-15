3fa4e15a19b0b321e50d333d3a2b4adee9028a8b
Predator <kungfuyoga99@gmail.com>
2026-03-15 20:40:07 +0530

    update

--- numpy12.py
+++ numpy12.py
@@ -1,7 +1,8 @@
 #Joining & Split NumPy Arrays Using
 import numpy as np
 
-var = np.array([1,2,3,4,5,6,7,8,9])
+var = np.array([1,2,3,4])
+var_1 = np.array([6,7,8,9])
 
 # for 1D joining array
-
+join =
