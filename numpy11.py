472a5398d9df7a01f26d575e7227ca3b94514c0c
Predator <kungfuyoga99@gmail.com>
2026-03-15 12:50:35 +0530

    update

--- numpy11.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-a = np.array([1,2,3], dtype=np.int16)
-b = np.array([10,20,30], dtype=np.int64)
-
-it = np.nditer([a,b], flags=['common_dtype'])
-
-for x,y in it:
-    print(type(x), type(y))
