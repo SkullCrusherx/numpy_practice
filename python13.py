8fbe7f89d6399cad6a9179438b5ffd1c9d366eea
Predator <kungfuyoga99@gmail.com>
2026-03-15 23:00:05 +0530

    update

--- python13.py
+++ python13.py
@@ -1,12 +1,9 @@
 import numpy as np
 
-var = np.array([1,2,3,4,5])
-var_1 = np.array([5,6,7,8,9])
-var_2 = np.array([10,11,12,13,14])
-var_3 = np.array([16,17,18,19,20])
+var = np.array([[1,2,3,4,5]])
+var_1 = np.array([[5,6,7,8,9]])
 
-new_nor = np.stack((var,var_1,var_2,var_3),axis=0)
+
+
+new_nor = np.dstack((var,var_1))
 print(f"new normal :\n",new_nor)
-
-new_ver= np.stack((var,var_1,var_2,var_3),axis=1)
-print(f"new normal :\n",new_ver)
