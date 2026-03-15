5b8250ca82d903f5fca64c24a274a874713f35bd
Predator <kungfuyoga99@gmail.com>
2026-03-15 21:10:46 +0530

    update

--- numpy12.py
+++ numpy12.py
@@ -7,8 +7,8 @@ var_1 = np.array([6,7,8,9])
 var_3 = np.array([[1,2,3],[4,5,6]])
 var_4 = np.array([[7,8,9],[7,6,5]])
 
-var_5 = np.array([[1,2,3],[4,5,6]])
-var_6 = np.array([[7,8,9],[7,6,5]])
+var_5 = np.array([[[1,2,3]],[[4,5,6]],[[7,8,9]]])
+var_6 = np.array([[[7,8,9]],[[2,3,4]],[[7,6,5]]])
 
 # for 1D joining array
 join = np.concatenate((var,var_1))
