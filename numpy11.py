41b2b14cb70f2575b5e76c3280b1478cac376daa
Predator <kungfuyoga99@gmail.com>
2026-03-15 16:00:52 +0530

    update

--- numpy11.py
+++ numpy11.py
@@ -5,8 +5,9 @@ import numpy as np
 var = np.array([1,2,3,4,5,6])
 var_copy = var.copy() #original copy
 
-var[0]=19
+var[0]=19 #changes the var elemnt index number of 0
 print(var_copy)
 
+#view function direct copy original function and if changes show as it is
 var_view = var.view()
 print(var_view)
