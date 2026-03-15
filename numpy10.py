296d76730e9a6d72d4b2d3682220d8eabbf3cad4
Predator <kungfuyoga99@gmail.com>
2026-03-15 13:24:55 +0530

    update

--- numpy10.py
+++ numpy10.py
@@ -1,22 +1,22 @@
-# Iterration of numpy array
+# Iteration of numpy array
 import numpy as np
 
 var = np.array([1,2,3,4,5,6])
 var_2d = np.array([[1,2,3,4,5,6],[9,8,7,6,5,4]])
 var_3d = np.array([[[1,2,3,4,5,6]],[[3,4,5,6,7,8]],[[4,5,6,7,8,9]]])
 
-#for 1D one for loop for 2D two for loop for 3D three for loop for 4D for four loop
+# For 1D one for loop for 2D two for loop for 3D three for loop for 4D for four loop
 for x in var_2d:
     for item in x:
         print(item)
 
-#funtion nditer() using here
+# Funtion nditer() using here
 for x in np.nditer(var):
     print(x)
 
-#funtion nditer() change data type using here
-#using flags for extra space from memory
-# using OP_dtype for byte string "S"
+# Funtion nditer() change data type using here
+# Using flags for extra space from memory
+# Using OP_dtype for byte string "S"
 # If only need int or other than using casting to change it like these Casting=["unsafe"]
 for x in np.nditer(var_3d,flags=["buffered"],op_dtypes=['U'],casting="unsafe"):
     print(x)
