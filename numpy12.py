0fe4ad749c10972ae2b7c002543643302877cc1f
Predator <kungfuyoga99@gmail.com>
2026-03-15 21:11:47 +0530

    update

--- numpy12.py
+++ numpy12.py
@@ -23,5 +23,5 @@ print(join_2D_col)
 # for 3D joining array
 join_3D_row = np.concatenate((var_3,var_4),axis=1)
 join_3D_col = np.concatenate((var_3,var_4),axis=0)
-print(join_2D_row)
-print(join_2D_col)
+print(join_3D_row)
+print(join_3D_col)
