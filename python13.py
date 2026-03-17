943eb00c787dbc1aea1c5b458a019c4e1e4197b2
Predator <kungfuyoga99@gmail.com>
2026-03-17 11:13:07 +0530

    update

--- python13.py
+++ python13.py
@@ -6,6 +6,10 @@ var = np.array([1,2,3,4,5,6])
 finding_from_arr = np.where(var%2 == 1)
 print(finding_from_arr)
 
-#Checking the number index and condition where given input send and checking side right and left
-insert_element_sort = np.searchsorted(var,100,side='right')
+#Checking the number index and condition where given input send and checking side left to right & retun the index number
+#from left to right
+insert_element_sort = np.searchsorted(var,100)
+insert_element_sort_r = np.searchsorted(var,100,side = 'right') #from right to left
+
+print(insert_element_sort_r)
 print(insert_element_sort)
