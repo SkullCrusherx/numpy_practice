045289e717c5f86d420e4f27962759de42f43019
Predator <kungfuyoga99@gmail.com>
2026-03-17 10:37:31 +0530

    update

--- python13.py
+++ python13.py
@@ -6,6 +6,6 @@ var = np.array([1,2,3,4,5,6])
 finding_from_arr = np.where(var%2 == 1)
 print(finding_from_arr)
 
-#checking the number index and condition where given input send
+#checking the number index and condition where given input send and
 insert_element_sort = np.searchsorted(var,100,side='right')
 print(insert_element_sort)
