d40e957f71e7c686f7ff4668538e299b12015650
Predator <kungfuyoga99@gmail.com>
2026-03-18 21:38:15 +0530

    update

--- python13.py
+++ python13.py
@@ -2,14 +2,17 @@
 import numpy as np
 
 #for checking condition or number index number
-var = np.array([1,2,3,4,5,6])
+var = np.array([1,2,3,4,5,6,5,8,6,9])
 finding_from_arr = np.where(var%2 == 1)
 
 #Checking the number index and condition where given input send and
-# checking side left to right & retun the index number
+#Checking side left to right & return the index number
 #from left to right
 insert_element_sort = np.searchsorted(var,100)
 insert_element_sort_r = np.searchsorted(var,100,side = 'right') #from right to left
+
+#sorting alphabet and also number
+
 
 print(finding_from_arr)
 print(insert_element_sort_r)
