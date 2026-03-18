611987c0880f3b203672f01a963e4819076aac22
Predator <kungfuyoga99@gmail.com>
2026-03-18 23:23:00 +0530

    New Update

--- python13.py
+++ python13.py
@@ -23,14 +23,20 @@ alphabet_sorting = np.sort(alphabet)
 
 #Reverse sorting number
 Reverse_sorting = np.sort(var)[::-1] #For 1D
+Reverse_sorting_2d = np.sort(var_2d)[::-1]
 
 #Reverse alphabet sorting
 Reverse_alphabet_sorting = np.sort(alphabet)[::-1] #For 1D
+Reverse_alphabet_sorting_2d = np.sort(alphabet_2d)[::-1]
 
+print("*"*100)
 print("search index number of array according to condition : ",finding_from_arr)
 print("index check from right to left : ",insert_element_sort_r)
 print("index check from left to right : ",insert_element_sort)
-print("sorting : ",sorting)
-print("alphabet sorting : ",alphabet_sorting)
-print("reverse sorting : ",Reverse_sorting)
-print("reverse alphabet sorting : ", Reverse_alphabet_sorting)
+print("*"*100)
+print("sorting 1D: ",sorting)
+print("alphabet sorting 1D: ",alphabet_sorting)
+print("reverse sorting 1D: ",Reverse_sorting)
+print("reverse alphabet sorting 1D: ", Reverse_alphabet_sorting)
+print("reverse number sorting 2D: ", Reverse_sorting_2d)
+print("reverse alphabet sorting 2D: ", Reverse_alphabet_sorting_2d)
