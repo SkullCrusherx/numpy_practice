16f16a239c9f5d41a56572f03ba6bdc0620b9c54
Sayan Lodh <33943721+SkullCrusherx@users.noreply.github.com>
2026-03-18 13:54:06 +0530

    Refactor print statements and comments in python13.py
    Updated print statements for clarity and added comments.

--- python13.py
+++ python13.py
@@ -1,15 +1,14 @@
 # Search ,Sort , Search Sorted , Filter
 import numpy as np
 
-#for checking condition or number index number
 var = np.array([1,2,3,4,5,6])
+#for checking condition or number index number
 finding_from_arr = np.where(var%2 == 1)
-print(finding_from_arr)
 
 #Checking the number index and condition where given input send and checking side left to right & retun the index number
-#from left to right
-insert_element_sort = np.searchsorted(var,100)
+insert_element_sort = np.searchsorted(var,100) #from left to right
 insert_element_sort_r = np.searchsorted(var,100,side = 'right') #from right to left
 
-print(insert_element_sort_r)
-print(insert_element_sort)
+print("search : ",finding_from_arr)
+print("cheking sorting index left to right : ",insert_element_sort)
+print("cheking sorting index right to left :",insert_element_sort_r)
