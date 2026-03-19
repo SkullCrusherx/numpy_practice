11b2af9bdc4ad6896bbaec9a22990fd6080f6338
Predator <kungfuyoga99@gmail.com>
2026-03-19 09:58:37 +0530

    New Update

--- numpy14.py
+++ numpy14.py
@@ -4,6 +4,8 @@ import numpy as np
 var = np.array([1,2,3,4,5,6,4,6,7,8,2,9])
 var_2d = np.array([[1,2,3],[7,8,9]])
 
+#----------------Shuffle----------------------------
+
 np.random.shuffle(var) #no need to store it return Value
 np.random.shuffle(var_2d)
 
