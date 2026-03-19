e15674f0564515f59aa6c0148a87617f65b18b38
Predator <kungfuyoga99@gmail.com>
2026-03-19 20:27:14 +0530

    New Update

--- numpy14.py
+++ numpy14.py
@@ -8,7 +8,7 @@ var_2d = np.array([[1,2,3],[7,8,9]])
 #----------------Shuffle----------------------------
 
 np.random.shuffle(var) #no need to store it return Value
-np.random.shuffle(var_2d)
+np.random.shuffle(var_2d) #no need to store it return Value
 
 #--------------------------------------------------------
 #----------------Unique----------------------------------
