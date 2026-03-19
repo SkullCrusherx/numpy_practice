69ed2df5bab79e5eab3fe1a8bbbd93a79e5cf0b2
Predator <kungfuyoga99@gmail.com>
2026-03-19 09:57:40 +0530

    New Update

--- numpy14.py
+++ numpy14.py
@@ -1,8 +1,11 @@
 # Shuffle() # Unique() # Resize() # Flatten() # Ravel()
 import numpy as np
 
-var = np.array([1,2,3,4,5,6])
+var = np.array([1,2,3,4,5,6,4,6,7,8,2,9])
+var_2d = np.array([[1,2,3],[7,8,9]])
 
-np.random.shuffle(var)
-print(var)
+np.random.shuffle(var) #no need to store it return Value
+np.random.shuffle(var_2d)
+
+
 
