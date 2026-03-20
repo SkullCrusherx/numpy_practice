5113676b14b9f06ce33759f109b5db08de57d4d2
Predator <kungfuyoga99@gmail.com>
2026-03-20 09:45:18 +0530

    auto commit

--- numpy15.py
+++ numpy15.py
@@ -4,6 +4,8 @@ import numpy as np
 var = np.array([1,2,3,4,5])
 var_2d = np.array([[1,2,3],[4,5,6]])
 
-insrt = np.insert(var,5,[7,8,9])
+#np.insert(array,index number ,element or list)
+insrt = np.insert(var,5,[7,8,9]) #for 1D usage insert by index number
+
 appnd = np.append(var,[10,11,12])
 dd = np.append(var,[13,14,15])
