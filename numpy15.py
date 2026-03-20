583d1684086f76d31d3ea7679f3d1f2bde9ce89b
Predator <kungfuyoga99@gmail.com>
2026-03-20 09:59:40 +0530

    auto commit

--- numpy15.py
+++ numpy15.py
@@ -5,13 +5,14 @@ var = np.array([1,2,3,4,5])
 var_2D = np.array([[1,2,3],[4,5,6]])
 var_3D = np.array([[[1,2,3]],[[5,4,7]],[[4,5,6]]])
 
-#np.insert(array,index number ,element or list)
+#======================================================================================
+#==================np.insert(array,index number ,element or list)======================
+#======================================================================================
 insrt = np.insert(var,5,[7,8,9]) #for 1D usage insert by index number
-#np.insert(array, element or list)
-appnd = np.append(var,[10,11,12]) #for 1d insert data or append at the last side of array
 
 var_2d = np.insert(var_2D,0,[7,8,9],axis=0)
 var_3d = np.insert(var_3D,0,[7,8,9],axis=1)
 
 print("for 2D output :",var_2d)
 print("for 3D output :",var_3d)
+
