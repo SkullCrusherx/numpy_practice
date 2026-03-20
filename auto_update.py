ceee72fe1afc6267bd8147fa5bd61ce174af77e1
Predator <kungfuyoga99@gmail.com>
2026-03-20 09:54:40 +0530

    auto commit

--- auto_update.py
+++ auto_update.py
@@ -5,4 +5,4 @@ while True:
     os.system("git add .")
     os.system('git commit -m "auto commit"')
     os.system("git push origin main")
-    time.sleep(10)
+    time.sleep(5)
--- numpy15.py
+++ numpy15.py
@@ -2,12 +2,16 @@
 import numpy as np
 
 var = np.array([1,2,3,4,5])
-var_2d = np.array([[1,2,3],[4,5,6]])
+var_2D = np.array([[1,2,3],[4,5,6]])
+var_3D = np.array([[[1,2,3]],[[5,4,7]],[[4,5,6]]])
 
 #np.insert(array,index number ,element or list)
 insrt = np.insert(var,5,[7,8,9]) #for 1D usage insert by index number
 #np.insert(array, element or list)
 appnd = np.append(var,[10,11,12]) #for 1d insert data or append at the last side of array
 
-var_2d = np.insert(var_2d,0,[7,8,9],axis=0)
+var_2d = np.insert(var_2D,0,[7,8,9],axis=0)
+print(var_2d)
+
+var_2d = np.insert(var_2D,0,[7,8,9],axis=0)
 print(var_2d)
