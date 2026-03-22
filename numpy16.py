bb7199857f82232df7516a21f69aca680caec391
Predator <kungfuyoga99@gmail.com>
2026-03-22 13:43:30 +0530

    auto commit

--- numpy16.py
+++ numpy16.py
@@ -2,7 +2,7 @@
 #MATRIX and matrix operations
 
 import numpy as np
-
+var_22 = np.matrix([[1,2],[4,5]])
 var = np.matrix([[1,2,3],[3,4,5]])
 var_2d = np.matrix([[4,5,6],[7,8,9]])
 
@@ -11,3 +11,5 @@ np.add(var,var_2d)
 print(var)
 print("===========")
 print(var.transpose())
+print(var_22.transpose())
+print(var_22.T)
