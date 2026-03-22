88becb7b79989cbfaba59dc85de3419862f666b3
Predator <kungfuyoga99@gmail.com>
2026-03-22 20:56:04 +0530

    auto commit

--- numpy16.py
+++ numpy16.py
@@ -2,8 +2,8 @@
 #MATRIX and matrix operations
 import numpy as np
 
-var = np.matrix([[1,2,3],[3,4,5]])
-var2 = np.matrix([[4,5,6],[7,8,9]])
+var = np.matrix([[1,2],[4,5]])
+var2 = np.matrix([[4,5],[8,9]])
 
 add = np.add(var,var2)
 sub = np.subtract(var,var2)
