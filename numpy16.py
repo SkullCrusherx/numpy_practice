8a8a08750a71e076e5904a8ff0cb4e3c82abda20
Predator <kungfuyoga99@gmail.com>
2026-03-22 20:11:27 +0530

    auto commit

--- numpy16.py
+++ numpy16.py
@@ -6,9 +6,7 @@ var_1D = np.array([[1,2],[5,6]])
 var_22 = np.matrix([[1,2],[4,5]])
 var = np.matrix([[1,2,3],[3,4,5]])
 var_2d = np.matrix([[4,5,6],[7,8,9]])
-
 """np.add(var,var_2d)
-
 print(var)
 print("=================================")
 print(var.transpose())
