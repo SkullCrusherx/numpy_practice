78dd2a79cb444718075cee57416364d6996793ac
Predator <kungfuyoga99@gmail.com>
2026-03-22 20:08:38 +0530

    auto commit

--- numpy16.py
+++ numpy16.py
@@ -2,11 +2,12 @@
 #MATRIX and matrix operations
 import numpy as np
 
+var_1D =
 var_22 = np.matrix([[1,2],[4,5]])
 var = np.matrix([[1,2,3],[3,4,5]])
 var_2d = np.matrix([[4,5,6],[7,8,9]])
 
-np.add(var,var_2d)
+"""np.add(var,var_2d)
 
 print(var)
 print("=================================")
@@ -20,4 +21,7 @@ print(var.swapaxes(1,1))
 print("=================================")
 print(var.swapaxes(0,0))
 print("=================================")
-print(var.swapaxes(0,1))
+print(var.swapaxes(0,1))"""
+
+F = np.linalg.solve(var,var_22)
+print(F)
