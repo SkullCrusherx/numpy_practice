cfd12c610770b7deae57f7cf58e6b873bc5afaf5
Predator <kungfuyoga99@gmail.com>
2026-03-22 19:22:55 +0530

    auto commit

--- numpy16.py
+++ numpy16.py
@@ -1,7 +1,7 @@
 #Last series of WS cube Tutorial
 #MATRIX and matrix operations
-
 import numpy as np
+
 var_22 = np.matrix([[1,2],[4,5]])
 var = np.matrix([[1,2,3],[3,4,5]])
 var_2d = np.matrix([[4,5,6],[7,8,9]])
