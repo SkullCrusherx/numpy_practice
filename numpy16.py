f97da297190ea43a2b83860aab9919e41e041b6e
Predator <kungfuyoga99@gmail.com>
2026-03-22 20:41:54 +0530

    auto commit

--- numpy16.py
+++ numpy16.py
@@ -3,12 +3,6 @@
 import numpy as np
 
 var = np.matrix([[1,2,3],[3,4,5]])
-var_2d = np.matrix([[4,5,6],[7,8,9]])
+var2 = np.matrix([[4,5,6],[7,8,9]])
 
-np.add(var,var_2d)
-print(var)
-print("=================================")
-print(var.transpose())
-print("=================================")
-print(var.swapaxes(1,0))
-print("=================================")
+add = np.add(var,var2)
